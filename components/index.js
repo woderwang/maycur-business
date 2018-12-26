4c78031ac4cdbb9b65713ac8337d4b5553f8534e
lianjiewang <13325914779@163.com>
2018-12-26 10:32:28 +0800

    add new component pageheaderbar

--- components/index.js
+++ components/index.js
@@ -1,10 +1,13 @@
+import './style/common.less';
 import MkTable from './MkTable';
 import Avatar from './Avatar';
 import MkLayout from './MkLayout';
 import Empty from './Empty';
+import PageHeaderBar from './PageHeaderBar';
 export {
     MkTable,
     Avatar,
     MkLayout,
-    Empty
+    Empty,
+    PageHeaderBar
 }
